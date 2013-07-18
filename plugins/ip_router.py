e392e8ae6caa41100f581912c80c7be292a2b11f
ttwait <ttwait@gmail.com>
2013-07-18 14:28:47 +0800

    fix bug

--- plugins/ip_router.py
+++ plugins/ip_router.py
@@ -7,7 +7,10 @@ old_socket = socket.socket
 def new_socket(*args, **kwargs):
     from core import proxystate
     sock = old_socket(*args, **kwargs)
-    sock.bind((proxystate.thread_local.server_address[0], 0))
+    try:
+        sock.bind((proxystate.thread_local.server_address[0], 0))
+    except AttributeError:
+        pass
     return sock
 
 socket.socket = new_socket
